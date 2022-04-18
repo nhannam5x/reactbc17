352f121ba160e0cf2efcfe54c3d4aebc0a7e1225
nhannam5x <do.nhannam5x@gmail.com>
2022-04-18 20:43:30 +0700

    thunk????

--- src/pages/Hooks/ToDoListHook/ToDoListHook.jsx
+++ src/pages/Hooks/ToDoListHook/ToDoListHook.jsx
@@ -1,82 +1,84 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useSelector,useDispatch } from 'react-redux';
-import { getApiTaskAction } from '../../../redux/actions/toDoListAction';
+import { getApiArrTaskAction } from '../../../redux/actions/toDoListAction';
 
 export default function ToDoListHook() {
 
     // const [arrTask,setArrTask] = useState([]);
-const {arrTask} = useSelector(state => state.toDoListReducer); //state ở đây chính là rootreducer
-const dispatch = useDispatch();
-    useEffect( async ()=>{
-    //     try{
-    //     //gọi api
-    //     const result = await axios({
-    //         url: 'http://svcy.myclass.vn/api/ToDoList/GetAllTask',
-    //         method:'get'
-    //     })
+    const { arrTask } = useSelector(state => state.toDoListReducer);
+    const dispatch = useDispatch();
+    useEffect(async () => {
+        // try {
+        //     //gọi api
+        //     const result = await axios({
+        //         url: 'http://svcy.myclass.vn/api/ToDoList/GetAllTask',
+        //         method: 'get'
+        //     });
+        //     // console.log(result.data);
+        //     // await setArrTask(result.data);
+        //     const action = {
+        //         type: 'GET_ALL_TASK_API',
+        //         arrTask: result.data
+        //     }
+        //     dispatch(action);            
+        // } catch (err) {
 
-    //     // console.log(result.data);
-    //     //await setArrTask(result.data)
-    //     const action = {
-    //         type:"GET_ALL_TASK_API",
-    //         arrTask: result.data
-    //     } 
-        
-    //     dispatch(action);
-    // }catch(err){
-    //     // console.log({err});
-    //     // if(err.response.status == 404) {
-    //     //     props.history.push('/');
-    //     // }
-    // }
+        // } 
 
-    /*
-        Trong redux có 2 dạng action
-        + action: {type:'',payload:'data_gui_id'}
-        + action: (dispatch) => {xử lý logic => dispatch}
-    */
-   const action = getApiTaskAction;
-   dispatch(action);
-    },[])
+        /*
+            Trong redux có 2 dạng action 
+            + action : {type:'',payload:'data_gui_di'}
+            + action : (dispatch) => { xử lý logic => dispatch}
+        */
+        const action = getApiArrTaskAction();
 
-  return (
-    <div className='container'>
-    <h3 className='text-center'>To do app</h3>
-    <div className='card'>
-        <div className='card-header'>
-            <div className='input-group-prepend mb-3'>
-                <span className='input-group-text btn btn-success'>Add task</span>
-                <input className='form-control' id='taskName'/>
+        dispatch(action);
+
+    }, [])
+
+    //ctr | cmd + F
+
+    return (
+        <div className='container'>
+            {/*Xoá button get task api */}
+            <h3 className='text-center'>To do app</h3>
+            <div className='card'>
+                <div className='card-header'>
+                    <div className='input-group-prepend mb-3'>
+                        <span className='input-group-text btn btn-success'>Add task</span>
+                        <input className='form-control' id="taskName" />
+                    </div>
+                </div>
+                <div className='card-body'>
+                    <table className='table'>
+                        <tbody>
+                            {/* Load công việc chưa làm */}
+                            {arrTask.filter(task => task.status === false).map((task, index) => {
+                                return <tr key={index}>
+                                    <td>{task.taskName}</td>
+                                    <td>
+                                        <span className='badge badge-danger'>incomplete</span>
+                                    </td>
+                                </tr>
+                            })}
+                        </tbody>
+                        <tfoot>
+                            {/* Load công việc làm rồi */}
+                            {arrTask.filter(task => task.status === true).map((task, index) => {
+                                return <tr key={index}>
+                                    <td>{task.taskName}</td>
+                                    <td>
+                                        <span className='badge badge-success'>complete</span>
+                                    </td>
+                                </tr>
+                            })}
+
+                        </tfoot>
+                    </table>
+                </div>
             </div>
-            <div className='card-body'>
-                <table className='table'>
-                    <tbody>
-                        {/*Load công việc chưa làm */}
-                        { arrTask.filter(task => task.status === false).map((task,index)=>{
-                              return <tr key={index}>
-                                  <td>{task.taskName}</td>
-                                  <td>
-                                      <span className='badge badge-danger'>incomplete</span>
-                                  </td>
-                              </tr>
-                          })}
-                    </tbody>
-                    <tfoot>
-                        {/*Load công việc làm rồi */}
-                          { arrTask.filter(task => task.status === true).map((task,index)=>{
-                              return <tr key={index}>
-                                  <td>{task.taskName}</td>
-                                  <td>
-                                      <span className='badge badge-success'>complete</span>
-                                  </td>
-                              </tr>
-                          })}
-                    </tfoot>
-                </table>
-            </div>
+
         </div>
-    </div>
-</div>
-  )
+    )
 }
--- src/redux/actions/toDoListAction.js
+++ src/redux/actions/toDoListAction.js
@@ -23,25 +23,24 @@ import axios from "axios";
 //      }
 //     }
 // }
-export const getApiTaskAction = async (dispatch) =>{
-    //Xử lý bất đồng bộ rồi mới có dữ liệu lên redux = tham số dispatch
-    try{
-         const result = await axios({
-             url: 'http://svcy.myclass.vn/api/ToDoList/GetAllTask',
-             method:'GET'
-         })
- 
-         // console.log(result.data);
-         //await setArrTask(result.data)
-         const action = {
-             type:"GET_ALL_TASK_API",
-             arrTask: result.data
-         } 
-         
-         dispatch(action);
-     }catch(err){
-         console.log(123);
-     }
+export const getApiArrTaskAction = () => {
+    return async (dispatch) => {
+        //Xử lý bất đồng bộ rồi mới có dữ liệu gửi lên redux = tham số dispatch
+        try {
+            //gọi api
+            const result = await axios({
+                url: 'http://svcy.myclass.vn/api/ToDoList/GetAllTask',
+                method: 'get'
+            });
+            // console.log(result.data);
+            // await setArrTask(result.data);
+            const action = {
+                type: 'GET_ALL_TASK_API',
+                arrTask: result.data
+            }
+            dispatch(action);
+        } catch (err) {
+
+        }
+    }
 }
-
-
